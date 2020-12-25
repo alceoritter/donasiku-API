bbd1ca9f64cf0a887e1eb97681061364fd3ae767
alceoritter <alceoritter@gmail.com>
2020-12-25 08:57:15 +0700

    update controller

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\RequestException;
 
 use Illuminate\Http\Request;
 use Kreait\Firebase\Factory;
 use Kreait\Firebase\ServiceAccount;
 use Kreait\Firebase;
+use PhpParser\Node\Expr\Cast\Double;
 
 class NotificationController extends Controller
 {
@@ -27,10 +30,9 @@ class NotificationController extends Controller
       $order_id = $notif_body['order_id'];
       $transaksi_id = $notif_body['transaction_id'];
       $status_code = $notif_body['status_code'];
+      $gross_amount = $notif_body['gross_amount'];
 
-      $ref = 'Transaksi%20Pembayaran/'. $order_id ;
-
-      
+      $ref = 'Transaksi%20Pembayaran/' . $order_id;
 
       $data = [
         'status_code' => $status_code
@@ -38,22 +40,107 @@ class NotificationController extends Controller
       $database->getReference($ref)->update($data);
 
 
-      // if (!$ref) {
-      //   return ['code' => 0, 'message' => 'Terjadi kesalahan. Order tidak ditemukan'];
-      // }
-      // switch ($ref) {
-      //   case '200': // sukses
+      if (!$status_code) {
+        return ['code' => 0, 'message' => 'Terjadi kesalahan. Order tidak ditemukan'];
+      } else {
+        switch ($status_code) {
+          case '200': // sukses
+            // get jenis donasi
+            $product_name = $database->getReference($ref)->getSnapshot()->getChild('product_name')->getValue();
 
-      //     break;
-      //   case '201': // pending
+            if ($product_name == 'Donasi Umum') {
+              // donasi umum
+              $ref_donasi = 'Dana/-MKcKLqNL6jEgBgnNg8p';
 
-      //     break;
-      //   case '202': // cancel
+              $dana_tersedia = $database->getReference($ref_donasi)->getSnapshot()->getChild('total_dana')->getValue();
 
-      //     break;
-      // }
+              $parse_dana_tersedia = floatval($dana_tersedia);
+              $data = [
+                'total_dana' => floatval($gross_amount) + $parse_dana_tersedia
+              ];
+              $database->getReference($ref_donasi)->update($data);
+            } else {
 
-      return response('OK', 200)->header('Content-type', 'text/plain');
+
+              // get biaya kebutuhan
+              $ref_kebutuhan = 'Bayar%20Kebutuhan/' . $product_name;
+              $sisa_nominal_kebutuhan = $database->getReference($ref_kebutuhan)->getSnapshot()->getChild('sisa_nominal_kebutuhan')->getValue();
+              $parse_sisa_nominal_kebutuhan = floatval($sisa_nominal_kebutuhan);
+
+              // mengurangi sisa nominal awal
+              $sisa_nominal = $parse_sisa_nominal_kebutuhan - floatval($gross_amount);
+
+              // jika berlebih maka simpan ke tabungan masjid
+              if ($sisa_nominal < 0) {
+                $to_positif = $sisa_nominal * -1;
+                // set sisa nominal kebutuhan menjadi 0
+                $data = [
+                  'sisa_nominal_kebutuhan' => "0"
+                ];
+                $database->getReference($ref_kebutuhan)->update($data);
+
+                // get id pengurus
+                $ref_idPengurus = 'Kebutuhan/' . $product_name;
+                $id_pengurus = $database->getReference($ref_idPengurus)->getSnapshot()->getChild('id_pengurus')->getValue();
+
+                // cek apakah id pengurus sudah ada atau belum
+                $query = $database->getReference('Dana')
+                  ->getChildKeys();
+
+                $already = true;
+
+                foreach ($query as $key => $value) {
+                  try {
+                    $ref_ = 'Dana/' . $value;
+                    $cek = $database->getReference($ref_)->getValue();
+                    if ($cek['id_pengurus'] == $id_pengurus) {
+                      $data_dana = [
+                        'total_dana' => $to_positif + floatval($cek['total_dana'])
+                      ];
+
+                      $database->getReference($ref_)->update($data_dana);
+                      $already = true;
+                      break;
+                    } else {
+                      $already = false;
+                      continue;
+                    }
+                  } catch (ClientException $cl) {
+                    return $cl;
+                  } catch (RequestException $rq) {
+                    return $rq;
+                  }
+                }
+
+                // jika tidak ada maka tambah baru 
+                if ($already == false) {
+
+                  $upload = [
+                    'id_pengurus' => $id_pengurus,
+                    'total_dana' => $to_positif,
+                    'jenis_dana' => 'Dana Lebih'
+                  ];
+
+                  $database->getReference('Dana/')->push($upload);
+                }
+              } else {
+                $data = [
+                  'sisa_nominal_kebutuhan' => strval($sisa_nominal)
+                ];
+                $database->getReference($ref_kebutuhan)->update($data);
+              }
+            }
+            break;
+          case '201': // pending
+
+            break;
+          case '202': // cancel
+
+            break;
+        }
+      }
+
+      return response("OK", 200)->header('Content-type', 'text/plain');
     } catch (\Throwable $th) {
       dd($th);
       return ['code' => 0, 'message' => 'Terjadi kesalahan'];
